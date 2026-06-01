ccb4ebac9a40fedcc54722491cc3ac66e835305c
Smyrnov Vlad <vladsimovv@gmail.com>
2026-06-02 00:24:46 +0300

    fix: add 'manufacturer' property to Product

--- init_sql/init.sql
+++ init_sql/init.sql
@@ -7,6 +7,7 @@ CREATE TABLE "Product" (
     id_product INT PRIMARY KEY,
     category_number INT NOT NULL,
     product_name VARCHAR(50) NOT NULL,
+    manufacturer VARCHAR(50) NOT NULL,
     characteristics VARCHAR(100) NOT NULL,
     FOREIGN KEY (category_number) REFERENCES "Category"(category_number) ON UPDATE CASCADE ON DELETE NO ACTION
 );
